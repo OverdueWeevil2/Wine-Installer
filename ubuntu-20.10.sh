656d54d57b5cd9840ea2ce5ed0943f36eeaa993d
OverdueWeevil2 <61hasanabdullahuygun@gmail.com>
2021-03-04 17:12:15 +0300

    Create ubuntu-20.10.sh

---
+++ ubuntu-20.10.sh
@@ -0,0 +1,12 @@
+echo Installing Wine for Ubuntu 20.10
+sudo dpkg --add-architecture i386 
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo apt-key add winehq.key
+rm winehq.key
+rm -f winehq.key
+sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ groovy main'
+echo Updating packages...
+sudo apt update
+echo Installing Wine package...
+sudo apt install --install-recommends winehq-staging
+echo Installed Wine!
