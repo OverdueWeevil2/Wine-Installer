06253e192c93764b0d50ea2698a98502fe8202c2
OverdueWeevil2 <61hasanabdullahuygun@gmail.com>
2021-03-05 21:02:36 +0300

    Update Debian-10-(Buster).sh

--- Debian/Debian-10-(Buster).sh
+++ Debian/Debian-10-(Buster).sh
@@ -1,8 +1,12 @@
+echo Installing Wine for Debian 10 (Buster)
 sudo dpkg --add-architecture i386
 wget -nc https://dl.winehq.org/wine-builds/winehq.key
 sudo apt-key add winehq.key
 rm winehq.key
 rm -f winehq.key
 deb https://dl.winehq.org/wine-builds/debian/ buster main
+echo Updating packages...
 sudo apt update
+echo Installing Wine...
 sudo apt install --install-recommends winehq-staging
+echo Installed Wine!
