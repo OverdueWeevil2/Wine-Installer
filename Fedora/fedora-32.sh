7aa4b01e54f71739f553cbeb8dcb15510a229921
OverdueWeevil2 <61hasanabdullahuygun@gmail.com>
2021-03-05 21:03:13 +0300

    Update fedora-32.sh

--- Fedora/fedora-32.sh
+++ Fedora/fedora-32.sh
@@ -1,4 +1,4 @@
-echo Installing Wine for Fedora 32...
+echo Installing Wine for Fedora 32
 dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/32/winehq.repo
 dnf install winehq-staging
 echo Installed Wine!
