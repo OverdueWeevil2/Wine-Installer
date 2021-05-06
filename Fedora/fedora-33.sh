1367b9d5695b0fbf6954a989885b55badb225935
OverdueWeevil2 <61hasanabdullahuygun@gmail.com>
2021-05-06 16:42:56 +0300

    Update fedora-33.sh

--- Fedora/fedora-33.sh
+++ Fedora/fedora-33.sh
@@ -1,4 +1,5 @@
 echo Installing Wine for Fedora 33
 dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/33/winehq.repo
 dnf install winehq-staging
+dnf install winehq-staging -y
 echo Installed Wine!
